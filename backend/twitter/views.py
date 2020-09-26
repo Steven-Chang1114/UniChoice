3ab1a1bf9c8d3337f58339d641e312f14fd4780a
Prince Wang <princewang0201@berkeley.edu>
2020-09-26 14:43:17 -0700

    API for health index

--- backend/twitter/views.py
+++ backend/twitter/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.core import serializers
+from django.conf import settings
+import json
+from flask import jsonify
+import tweepy
+from textblob import TextBlob
+import pickle
+from .config import consumer_key, consumer_secret, access_token, access_token_secret
+
+# Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
 
 # Create your views here.
+@api_view(["GET"])
+def getHealthIndex(request):
+    count = 0
+    scoreSum = 0
+
+    for tweet in tweepy.Cursor(api.search, q=request.GET.get("text") + " covid" + " -filter:retweets", rpp=5, lang="en", tweet_mode='extended').items(50):
+        text = TextBlob(tweet.full_text)
+        scoreSum += text.sentiment.polarity
+        count += 1
+
+    avgScore = scoreSum / count
+    return JsonResponse({"score": avgScore});
