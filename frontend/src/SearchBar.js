aea8e248a35180ce50b7e043de336f443197e49c
Steven Chang <1632460272@qq.com>
2020-09-26 11:22:12 +0100

    Finishing up form

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -2,36 +2,38 @@ import React from 'react';
 import { makeStyles, styled } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const styles = {
   root: {
     '& > *': {
       width: '100%',
     },
-  },
-  text: {
-    ...theme.typography.button,
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(1),
-  },
-}));
+  }
+};
 
 const MyComponent = styled('div')({
   margin: "20.5vh 0",
 });
 
-export default function SearchBar() {
-  const classes = useStyles();
+@withStyles(styles)
+class SearchBar extends React.Component {
 
-  return (
-    <MyComponent>
-      <form className={classes.root} noValidate autoComplete="off">
-        <Typography variant="h2" align="center" gutterBottom>
-          University Search
-        </Typography>
-        <TextField id="outlined-basic" label="How's the COVID protection done by" variant="outlined" />
-      </form>
-    </MyComponent>
-
-  );
+  render(){
+    return (
+      <MyComponent>
+        <form className={this.props.classes.root} noValidate autoComplete="off" onSubmit={e => console.log("submit")}>
+          <Typography variant="h2" align="center" gutterBottom>
+            University Search
+          </Typography>
+          <TextField id="outlined-basic" label="How's the COVID protection done by" variant="outlined" />
+        </form>
+      </MyComponent>
+  
+    );
+  }
+  
 }
+
+
+export default SearchBar;
